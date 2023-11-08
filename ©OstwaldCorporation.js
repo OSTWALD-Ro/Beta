a5267da7534390811d648f3acc2cc3ed915565ec
OSTWALD-Ro <139101764+OSTWALD-Ro@users.noreply.github.com>
2023-11-08 09:08:15 -0600

    Add files via upload

--- ©OstwaldCorporation.js
+++ ©OstwaldCorporation.js
@@ -4,12 +4,14 @@
             var PB3 = "file:///D:/Files/Ostwald/JavaScript/Sistema/HTML/";
 
             var PG1A = "Menu_Arranque";
-            var PG1B = "Opciones_Arranque";
+            var PG1B = "Browser";
+            var PG2B = "Opciones_Arranque";
             var PG1C = "Menu_Arranque";
             var PG2C = "Menu_Arranque";
             
             var url1A = PB1 + PG1A;
             var url1B = PB1 + PG1B;
+            var url2B = PB1 + PG2B;
             var url1C = PB1 + PG1C;
             var url2C = PB1 + PG2C;
 
@@ -21,6 +23,7 @@
                 if (enLinea) {
                     url1A = PB1 + PG1A;
                     url1B = PB1 + PG1B;
+                    url2B = PB1 + PG2B;
                     url1C = PB1 + PG1C;
                     url2C = PB1 + PG2C;
                 } else {
@@ -28,11 +31,13 @@
                     if (enChromeOS) {
                         url1A = PB2 + PG1A + ".html";
                         url1B = PB2 + PG1B + ".html";
+                        url2B = PB2 + PG2B + ".html";
                         url1C = PB2 + PG1C + ".html";
                         url2C = PB2 + PG2C + ".html";
                   } else {
                         url1A = PB3 + PG1A + ".html";
                         url1B = PB3 + PG1B + ".html";
+                        url2B = PB3 + PG2B + ".html";
                         url1C = PB3 + PG1C + ".html";
                         url2C = PB3 + PG2C + ".html";
                   }
